df1c52fb755eb53d52818d3a208823c40a20e365
Jarkó Richárd <jarkoricsi16@gmail.com>
2025-03-02 01:05:39 +0100

    Light mode support added

--- resources/views/livewire/admin/create-new-user.blade.php
+++ resources/views/livewire/admin/create-new-user.blade.php
@@ -5,37 +5,37 @@
         </div>
     @endif
     @if($show_create_user_form)
-        <div class="bg-zinc-900 mx-auto max-w-md p-5 rounded-2xl">
+        <div class="mx-auto max-w-md p-5 bg-zinc-100 dark:bg-zinc-700 rounded-2xl">
             <form class="max-w-md mx-auto" wire:submit.prevent="createUser" autocomplete="off">
                 <div class="relative z-0 w-full mb-5 group">
-                    <input autocomplete="off" type="email" wire:model="email" name="floating_email" id="floating_email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer cursor-text" placeholder=" "  />
-                    <label for="floating_email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">E-mail cím</label>
+                    <input autocomplete="off" type="email" wire:model="email" name="floating_email" id="floating_email" class="block py-2.5 px-0 w-full text-sm text-zinc-900 bg-transparent border-0 border-b-2 border-zinc-300 appearance-none dark:text-white dark:border-zinc-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer cursor-text" placeholder=" "  />
+                    <label for="floating_email" class="peer-focus:font-medium absolute text-sm text-zinc-500 dark:text-zinc-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">E-mail cím</label>
                     @error('email') <span class="text-red-500 text-sm">{{ $message }}</span> @enderror
                 </div>
                 <div class="relative z-0 w-full mb-5 group">
-                    <input type="password" wire:model="password" name="floating_password" id="floating_password" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer cursor-text" placeholder=" "  />
-                    <label for="floating_password" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Jelszó</label>
+                    <input type="password" wire:model="password" name="floating_password" id="floating_password" class="block py-2.5 px-0 w-full text-sm text-zinc-900 bg-transparent border-0 border-b-2 border-zinc-300 appearance-none dark:text-white dark:border-zinc-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer cursor-text" placeholder=" "  />
+                    <label for="floating_password" class="peer-focus:font-medium absolute text-sm text-zinc-500 dark:text-zinc-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Jelszó</label>
                     @error('password') <span class="text-red-500 text-sm">{{ $message }}</span> @enderror
                 </div>
                 <div class="relative z-0 w-full mb-5 group">
-                    <input type="password" wire:model="confirm_password" name="repeat_password" id="floating_repeat_password" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer cursor-text" placeholder=" "  />
-                    <label for="floating_repeat_password" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Jelszó újra</label>
+                    <input type="password" wire:model="confirm_password" name="repeat_password" id="floating_repeat_password" class="block py-2.5 px-0 w-full text-sm text-zinc-900 bg-transparent border-0 border-b-2 border-zinc-300 appearance-none dark:text-white dark:border-zinc-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer cursor-text" placeholder=" "  />
+                    <label for="floating_repeat_password" class="peer-focus:font-medium absolute text-sm text-zinc-500 dark:text-zinc-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Jelszó újra</label>
                     @error('confirm_password') <span class="text-red-500 text-sm">{{ $message }}</span> @enderror
                 </div>
                 <div class="relative z-0 w-full mb-5 group">
-                    <input type="text" wire:model="name" name="floating_last_name" id="floating_last_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer cursor-text" placeholder=" " />
-                    <label for="floating_last_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Teljes név</label>
+                    <input type="text" wire:model="name" name="floating_last_name" id="floating_last_name" class="block py-2.5 px-0 w-full text-sm text-zinc-900 bg-transparent border-0 border-b-2 border-zinc-300 appearance-none dark:text-white dark:border-zinc-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer cursor-text" placeholder=" " />
+                    <label for="floating_last_name" class="peer-focus:font-medium absolute text-sm text-zinc-500 dark:text-zinc-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Teljes név</label>
                     @error('name') <span class="text-red-500 text-sm">{{ $message }}</span> @enderror
                 </div>
                 <div x-data="{ openSelect: false, selected: '', selected_role: '' }" class="relative z-50 w-full mb-5 group">
                     <div class="relative">
-                        <button type="button" @click="openSelect = !openSelect" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer flex justify-between items-center cursor-pointer">
-                            <span x-text="selected || 'Válassz egy lehetőséget'" class="text-gray-500 dark:text-gray-400"></span>
-                            <span class="text-gray-500 dark:text-gray-400">▼</span>
+                        <button type="button" @click="openSelect = !openSelect" class="block py-2.5 px-0 w-full text-sm text-zinc-900 bg-transparent border-0 border-b-2 border-zinc-300 dark:text-white dark:border-zinc-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer flex justify-between items-center cursor-pointer">
+                            <span x-text="selected || 'Válassz egy lehetőséget'" class="text-zinc-500 dark:text-zinc-400"></span>
+                            <span class="text-zinc-500 dark:text-zinc-400">▼</span>
                         </button>
-                        <ul x-show="openSelect" x-transition.opacity x-cloak @click.away="openSelect = false" class="absolute w-full mt-1 bg-gray-800 text-white border border-gray-600 rounded-md shadow-lg overflow-hidden z-50">
+                        <ul x-show="openSelect" x-transition.opacity x-cloak @click.away="openSelect = false" class="absolute w-full mt-1 bg-white dark:bg-zinc-800 text-zinc-800 dark:text-white border border-zinc-100 dark:border-zinc-600 rounded-md shadow-lg overflow-hidden z-50">
                             @foreach ($user_roles as $role)
-                                <li wire:click="$set('role', '{{ $role['value'] }}')" @click="selected = '{{ $role['display'] }}'; openSelect = false" class="px-4 py-2 hover:bg-gray-700 cursor-pointer">{{ $role['display'] }}</li>
+                                <li wire:click="$set('role', '{{ $role['value'] }}')" @click="selected = '{{ $role['display'] }}'; openSelect = false" class="px-4 py-2 hover:bg-zinc-100 dark:hover:bg-zinc-700 cursor-pointer">{{ $role['display'] }}</li>
                             @endforeach
                         </ul>
 
--- resources/views/livewire/admin/list-users.blade.php
+++ resources/views/livewire/admin/list-users.blade.php
@@ -1,9 +1,9 @@
 <div>
     <div class="p-6" wire:poll>
         <div class="overflow-x-auto">
-            <table class="min-w-full bg-zinc-800 text-white rounded-lg shadow-md">
+            <table class="min-w-full bg-white text-black dark:bg-zinc-800 dark:text-white rounded-lg shadow-md">
                 <thead>
-                    <tr class="bg-zinc-700 text-left">
+                    <tr class="bg-zinc-200 text-black dark:bg-zinc-700 dark:text-white text-left">
                         <th class="py-3 px-6">Név</th>
                         <th class="py-3 px-6">Email</th>
                         <th class="py-3 px-6">Szerep</th>
@@ -12,13 +12,13 @@
                 </thead>
                 <tbody>
                     @foreach ($users as $user)
-                        <tr class="{{ $loop->last === true ?: 'border-b' }} border-zinc-700 hover:bg-zinc-600">
+                        <tr class="border-b border-zinc-300 dark:border-zinc-700 hover:bg-zinc-100 dark:hover:bg-zinc-600">
                             <td class="py-3 px-6">{{ $user->name }}</td>
                             <td class="py-3 px-6">{{ $user->email }}</td>
                             <td class="py-3 px-6">{{ $user->role->display() }}</td>
                             <td class="py-3 px-6 text-right">
-                                <button class="text-blue-400 hover:text-blue-300 font-bold cursor-pointer">Szerkesztés</button>
-                                <button class="ml-3 text-red-400 hover:text-red-300 font-bold cursor-pointer">Törlés</button>
+                                <button class="text-blue-600 hover:text-blue-500 dark:text-blue-400 dark:hover:text-blue-300 font-bold cursor-pointer">Szerkesztés</button>
+                                <button class="ml-3 text-red-600 hover:text-red-500 dark:text-red-400 dark:hover:text-red-300 font-bold cursor-pointer">Törlés</button>
                             </td>
                         </tr>
                     @endforeach
