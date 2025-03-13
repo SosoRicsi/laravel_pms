c5c6330df05d848ff9bdaeafe3b728751b2005c7
Jarkó Richárd <jarkoricsi16@gmail.com>
2025-03-14 00:45:05 +0100

    Refactor alert component: slot-based content & improved success color

--- resources/views/components/form/message-box.blade.php
+++ resources/views/components/form/message-box.blade.php
@@ -1,18 +1,17 @@
 @props([
-    'type' => 'error',
-    'message',
+    'type' => '',
 ])
 @php
     $color = match ($type) {
         'error' => 'bg-red-500',
-        'success' => 'bg-zinc-600',
-        default => 'bg-orange-600',
+        'success' => 'bg-zinc-400 dark:bg-zinc-600',
+        default => 'bg-orange-500',
     };
 @endphp
 <div>
-    @if (isset($message) && !empty($message))
+    @if (isset($slot) && !empty($slot) && $slot != "")
         <div {{ $attributes->merge(['class' => "{$color} text-white text-center text-lg max-w-md mx-auto p-2 m-2 rounded-2xl font-bold"]) }}>
-            <p>{{ $message }}</p>
+            <p>{{ $slot }}</p>
         </div>
     @endif
 </div>
