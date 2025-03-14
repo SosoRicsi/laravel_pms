b8e9322fa66eba2bea5a5b0f3223f2b20d12833b
Jarkó Richárd <jarkoricsi16@gmail.com>
2025-03-14 13:26:00 +0100

    Move 'todos/show' route to the auth group

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use App\Http\Controllers\ManageUsersController;
 use App\Http\Controllers\SimpleTasksController;
+use Illuminate\Auth\Access\Gate;
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
@@ -21,12 +22,12 @@ Route::middleware(['auth'])->group(function () {
     Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
     Volt::route('settings/password', 'settings.password')->name('settings.password')->middleware('password.confirm');
     Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
+
+    Volt::route('/todos', 'dashboard.todo.show')->name('todos.index');
 });
 
 Route::middleware(['password.confirm', 'can:admin', 'auth', 'verified'])->group(function () {
     Route::view('/mange-users', 'dashboard.admin.manage-users')->name('manage-users');
 });
-
-Volt::route('/todos', 'dashboard.todo.show')->name('todos.index');
 
 require __DIR__.'/auth.php';
