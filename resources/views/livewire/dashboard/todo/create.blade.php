0547dce04cd2d9829c9a56778670c5705fb6c6ba
Jarkó Richárd <jarkoricsi16@gmail.com>
2025-03-14 13:59:34 +0100

    Create Livewire view for todo creation

---
+++ resources/views/livewire/dashboard/todo/create.blade.php
@@ -0,0 +1,50 @@
+<?php
+
+use App\Models\SimpleTasks;
+use Livewire\Volt\Component;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
+
+new class extends Component {
+    public string $title = "";
+
+    protected $messages = [
+        'title.required' => "Nem hagyhatod üresen a mezőt!",
+        'title.min' => "A címnek minimum :min karakterből kell álnia!",
+        'title.max' => "A cím nem lehet hosszabb :max karakternél!"
+    ];
+
+    protected $rules = [
+        'title' => [
+            'required',
+            'string',
+            'min:3',
+            'max:255'
+        ]
+    ];
+
+    public function create()
+    {
+        $validated = $this->validate();
+
+        $task = SimpleTasks::create([
+            'user_id' => Auth::id(),
+            'title' => $validated['title']
+        ]);
+
+        $this->dispatch('todos.reload_table');
+    }
+}; ?>
+
+<div>
+    <form class="max-w-lg mx-auto my-3 flex justify-between gap-3 bg-zinc-100 dark:bg-zinc-700 p-3 rounded-lg" wire:submit.prevent="create">
+        <div class="relative z-0 w-full mb-5 group">
+            <input autocomplete="off" type="text" wire:model="title" name="floating_title" id="floating_title" class="block py-2.5 px-0 w-full text-sm text-zinc-900 bg-transparent border-0 border-b-2 border-zinc-300 appearance-none dark:text-white dark:border-zinc-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer cursor-text" placeholder=" "  />
+            <label for="floating_title" class="peer-focus:font-medium absolute text-sm text-zinc-500 dark:text-zinc-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Feladat cím</label>
+            @error('title') <span class="text-red-500 text-sm">{{ $message }}</span> @enderror
+        </div>
+        <div class="flex justify-end">
+            <x-form.button size="base">Létrehozás</x-form.button>
+        </div>
+    </form>
+</div>
